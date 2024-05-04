5547989f8f5d94dd12535c08510712a2550e89b5
Anubhav <132827423+AnubhavXD@users.noreply.github.com>
2024-05-04 16:13:38 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -2,18 +2,18 @@ class Config(object):
     LOGGER = True
 
     # Get this value from my.telegram.org/apps
-    OWNER_ID = "6765826972"
-    sudo_users = "6845325416", "6765826972"
-    GROUP_ID = -1002133191051
-    TOKEN = "6707490163:AAHZzqjm3rbEZsObRiNaT7DMtw_i5WPo_0o"
-    mongo_url = "mongodb+srv://HaremDBBot:ThisIsPasswordForHaremDB@haremdb.swzjngj.mongodb.net/?retryWrites=true&w=majority"
+    OWNER_ID = "5512261555"
+    sudo_users = "5512261555", "6982003688"
+    GROUP_ID = -1002138258128
+    TOKEN = "7125549226:AAGufzEcBEhoHbcPo-x93ulDF8y6lQ6CjFY"
+    mongo_url = "mongodb://mongo:cpmRievpABDRrCVtXfXSSMDSOKcdCkpl@roundhouse.proxy.rlwy.net:58803"
     PHOTO_URL = ["https://telegra.ph/file/b925c3985f0f325e62e17.jpg", "https://telegra.ph/file/4211fb191383d895dab9d.jpg"]
-    SUPPORT_CHAT = "Collect_em_support"
-    UPDATE_CHAT = "Collect_em_support"
-    BOT_USERNAME = "Collect_Em_AllBot"
-    CHARA_CHANNEL_ID = "-1002133191051"
-    api_id = 26626068
-    api_hash = "bf423698bcbe33cfd58b11c78c42caa2"
+    SUPPORT_CHAT = "GuessTheCharacterGroup"
+    UPDATE_CHAT = "GuessTheCharacterUpdates"
+    BOT_USERNAME = "GuessTheMovieCharacterBot"
+    CHARA_CHANNEL_ID = "-1002040113200"
+    api_id = 21187167
+    api_hash = "f7c20f7bc2041887a70d62f94310e005"
 
     
 class Production(Config):
